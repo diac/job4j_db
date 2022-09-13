93ed46eeb3de00148908115fe24dfed9e1a55bb3
diac <nikolai.gladkikh@gmail.com>
2022-09-13 16:25:33 +0300

    Собрать все запросы в один скрипт и исправить замечания: Связи между таблицами [#373615 #342764]

---
+++ db/373615/exercise373615.sql
@@ -0,0 +1,32 @@
+create table person (
+	id serial primary key,
+	"name" VARCHAR(255),
+	age int,
+	registration_date date
+);
+
+create table account(
+    id serial primary key,
+    username varchar(255),
+    password varchar(255),
+    person_id int references person unique
+);
+
+create table subscription(
+    id serial primary key,
+    "name" varchar(255)
+);
+
+create table account_subscription(
+    id serial primary key,
+    account_id int references account(id),
+    subscription_id int references subscription("id"),
+    unique(account_id, subscription_id)
+);
+
+create table post(
+    id serial primary key,
+    title varchar(400),
+    "content" text,
+    account_id int references account(id)
+);
